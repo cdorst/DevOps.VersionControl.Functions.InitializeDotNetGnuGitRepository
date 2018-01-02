980e29a26bd5b46a2ade13d729a604033e30f5ae
Christopher Dorst <chris@dorst.email>
2018-01-02 02:51:39 -0600

    Add GitRepositoryInitializer

---
+++ DevOps.VersionControl.Functions.InitializeDotNetGnuGitRepository/GitRepositoryInitializer.cs
@@ -0,0 +1,18 @@
+﻿using DevOps.VersionControl.Structures.GitCommitUser;
+using System;
+using static DevOps.VersionControl.Functions.AddNewRepositoryFiles.NewRepositoryFilesAdder;
+using static DevOps.VersionControl.Functions.AddOriginGitRemoteAndPush.RemoteAdderAndChangePusher;
+using static DevOps.VersionControl.Functions.RunGitInitCommand.GitInitCommandRunner;
+
+namespace DevOps.VersionControl.Functions.InitializeDotNetGnuGitRepository
+{
+    public static class GitRepositoryInitializer
+    {
+        public static void InitializeRepository(string directory, Uri remote, UserInfo? user = null)
+        {
+            Init(directory);
+            AddAndCommitNewRepositoryFiles(directory, user);
+            AddRemoteAndPush(directory, remote);
+        }
+    }
+}
